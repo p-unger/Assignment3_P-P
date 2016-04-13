8669e822d90fe97bcc3347ee73064f2a39c7f414
p-unger <philip.unger@gmail.com>
2016-04-13 17:52:23 +0200

    Update to graphics and a very temporary markdown file

--- GSS_graphics.R
+++ GSS_graphics.R
@@ -2,8 +2,10 @@
 
 library(ggplot2)
 library(dplyr)
+library(plyr)
 library(repmis)
 library(MASS)
+library(Hmisc)
 
 # Distributional diagrams
 
@@ -38,6 +40,63 @@ hist(z$hrs1[z$working_ft==1],
      border = "White",
      xlab = "Hours worked last week (full-time employees)"
 )   
+
+####################################
+# Graph plots over years           #
+####################################
+
+
+# Prepare age x happiness
+z$vhappy1 <- as.numeric(z$vhappy)
+t <- z
+
+# Age and year - full sample
+ggplot(t, aes(x=factor(age), y=vhappy1)) + stat_summary(fun.y="mean", geom="bar", fill="navyblue")
+
+bp <- ggplot(t, aes(x=factor(age), y=vhappy1))
+bp + stat_summary(fun.y="mean", geom="line", fill="navyblue") + expand_limits(y=c(0.4))               
+bp + stat_summary(fun.y="mean", geom="point", col="Navyblue") + expand_limits(y=c(0.25,0.4))               
+
+ggplot(t, aes(x=factor(year), y=vhappy1)) + 
+  stat_summary(fun.y="mean", geom="point", col="Navyblue") + 
+  expand_limits(y=c(0.25,0.4))               
+
+# Age v. happiness (men & women seperate)
+female_educ <- subset(t, sex==2 & educat == 4)
+ggplot(female_educ, aes(x=factor(age), y=vhappy1)) + 
+  stat_summary(fun.y="mean", geom="point", col="Navyblue") + 
+  expand_limits(y=c(0.25,0.6))               
+
+
+male_educ <- subset(t, sex==1 & educat == 4)
+ggplot(male_educ, aes(x=factor(age), y=vhappy1)) + 
+  stat_summary(fun.y="mean", geom="point", col="Navyblue") + 
+  expand_limits(y=c(0.25,0.6))        
+
+
+##
+
+ggplot(t) + 
+  stat_summary(data = t[t$sex == 1,], aes(x=factor(age), y=vhappy1), 
+               fun.y="mean", geom="point", col="Navyblue") + 
+  stat_summary(data = t[t$sex == 2,], aes(x=factor(age), y=vhappy1), 
+               fun.y="mean", geom="point", col="Red") + 
+  expand_limits(y=c(0.25,0.4)) +
+  theme_bw()
+
+##
+
+aggdata <-aggregate(t, by=list(t$sex, t$age), 
+                            FUN=mean, na.rm=TRUE)
+
+ggplot(aggdata, aes(x=factor(age)), y=vhappy1) + geom_line()
+
+png(filename="/Users/Unger/Desktop/graphs/male_educ.png")
+plot(fit)
+dev.off()
+
+# Collapse on means
+
 
 # Regression attempts
 attach(z)
